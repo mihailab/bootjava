24bbc72595d897dcaffabb4f9c762e3bcdfeb2b7
mitriy <mitriy.spb@gmail.com>
2025-11-17 01:05:34 +0300

    3_02_jackson

--- src/main/java/ru/javaops/bootjava/model/BaseEntity.java
+++ src/main/java/ru/javaops/bootjava/model/BaseEntity.java
@@ -1,5 +1,6 @@
 package ru.javaops.bootjava.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 import org.springframework.data.domain.Persistable;
 import org.springframework.data.util.ProxyUtils;
@@ -28,6 +29,7 @@ public abstract class BaseEntity implements Persistable<Integer> {
         return id;
     }
 
+    @JsonIgnore
     @Override
     public boolean isNew() {
         return id == null;
